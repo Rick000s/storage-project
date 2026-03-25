2aa29aae69469037f9120eee5e7c00311e9a07fc
Andrii <rik24096@gmail.com>
2026-03-25 02:36:51 +0100

    Add cycle for main

--- src/main/java/cz/upce/fei/Main.java
+++ src/main/java/cz/upce/fei/Main.java
@@ -6,23 +6,48 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("menu: 1 insert db, 2 read db, 3 write file, 4 read file, 5 sync to db, 6 sync to file, 7 clear file, 8 clear db, j json, 0 exit");
 
-        char choice = sc.next().charAt(0);
+        String menu = "menu: 1 insert db, 2 read db, 3 write file, 4 read file, 5 sync to db, 6 sync to file, 7 clear file, 8 clear db, j json, 0 exit";
+
+        while (true) {
+            System.out.println(menu);
+            char choice = sc.next().charAt(0);
         switch (choice) {
-            case '0' -> System.exit(0);
-            case '1' -> DatabaseManager.simpleDataBase();
-            case '2' -> DatabaseManager.readFromSimpleDataBase();
-            case '3' -> FileManager.writeToTextFile();
-            case '4' -> FileManager.readFromTextFile();
-            case '5' -> SyncManager.syncFileToDatabase();
-            case '6' -> SyncManager.syncDatabaseToFile();
-            case '7' -> FileManager.clearFile();
-            case '8' -> DatabaseManager.clearDatabase();
-            case 'j' -> {
-                List<Task> tasks = DatabaseManager.getAllTasks();
-                JsonManager.saveTasksToJson(tasks);
-            }
+            case '0':
+                System.out.println("Вихід...");
+                System.exit(0);
+                break;
+            case '1':
+                DatabaseManager.simpleDataBase();
+                break;
+            case '2':
+                DatabaseManager.readFromSimpleDataBase();
+                break;
+            case '3':
+                FileManager.writeToTextFile();
+                break;
+            case '4':
+                FileManager.readFromTextFile();
+                break;
+            case '5':
+                SyncManager.syncFileToDatabase();
+                break;
+            case '6':
+                SyncManager.syncDatabaseToFile();
+                break;
+            case '7':
+                FileManager.clearFile();
+                break;
+            case '8':
+                DatabaseManager.clearDatabase();
+                break;
+            case 'j':
+                List<Task> currentTasks = DatabaseManager.getAllTasks(); // Дістали з бази
+                JsonManager.saveTasksToJson(currentTasks);               // Зберегли в JSON
+                break;
+                default:
+                    System.out.println("ERROR");
+        }
         }
     }
 }
